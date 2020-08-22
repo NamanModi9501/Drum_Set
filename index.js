c915dbf4e54cd80d70666cb2f56c95d230aed4f9
Naman Modi <62869432+NamanModi9501@users.noreply.github.com>
2020-08-22 11:02:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var audio=[];
+for(var i=0;i<4;i++)
+audio[i]=new Audio('tom-'+(i+1)+'.mp3');
+audio.push(new Audio('snare.mp3'));
+audio.push(new Audio('crash.mp3'));
+audio.push(new Audio('kick-bass.mp3'));
+document.addEventListener("keydown",function(event){
+  var k=event.key;
+  switch(k){
+    case "w": audio[0].play();break;
+    case "a": audio[1].play();break;
+    case "s": audio[2].play();break;
+    case "d": audio[3].play();break;
+    case "j": audio[4].play();break;
+    case "k": audio[5].play();break;
+    case "l": audio[6].play();break;
+  }
+});
+for(var i=0;i<7;i++)
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  var ch=this.innerHTML;
+  switch(ch){
+    case "w": audio[0].play();break;
+    case "a": audio[1].play();break;
+    case "s": audio[2].play();break;
+    case "d": audio[3].play();break;
+    case "j": audio[4].play();break;
+    case "k": audio[5].play();break;
+    case "l": audio[6].play();break;
+  }
+});
